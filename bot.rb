e807d7a021460942fddba2cccd11208a39c1d82f
Pavel Lobashov <shockwavenn@gmail.com>
2016-04-12 15:46:47 +0300

    Rubocop fixes

--- bot.rb
+++ bot.rb
@@ -2,15 +2,17 @@ require 'telegram/bot'
 require_relative 'github'
 config = YAML.load_file('config.yml')
 
-Telegram::Bot::Client.run(config['telegram_bot_token'], logger: Logger.new($stdout)) do |bot|
+Telegram::Bot::Client.run(config['telegram_bot_token']) do |bot|
   bot.listen do |message|
     case message.text
-      when '/get_changes'
-        text = ''
-        config['repos'].each do |cur_repo|
-          text += "#{get_changes_url(cur_repo)}\n"
-        end
-        bot.api.send_message(chat_id: message.chat.id, text: text, parse_mode: 'Markdown')
+    when '/get_changes'
+      text = ''
+      config['repos'].each do |cur_repo|
+        text += "#{get_changes_url(cur_repo)}\n"
+      end
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: text,
+                           parse_mode: 'Markdown')
     end
   end
 end
--- github.rb
+++ github.rb
@@ -7,16 +7,16 @@ Octokit.configure do |c|
   c.password = config['github_user_password']
 end
 
-def get_tag_names(repo)
+def get_tag_names(repo, tag_filter = '')
   tags = []
   Octokit.tags(repo).each do |current_tag|
-    tags << current_tag['name'] if current_tag['name'].include?('onlyoffice-documentserver')
+    tags << current_tag['name'] if current_tag['name'].include?(tag_filter)
   end
   tags
 end
 
 def get_changes_url(repo)
-  new_tags = get_tag_names(repo)
+  new_tags = get_tag_names(repo, 'onlyoffice-documentserver')
   new_tag = new_tags[0]
   old_tag = new_tags[1]
   "[#{repo} changes](http://github.com/#{repo}/compare/#{old_tag}...#{new_tag})"
