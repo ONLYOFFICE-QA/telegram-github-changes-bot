1c60b70b6629e9eb7da48c94f98a13d8d27741cf
Pavel Lobashov <shockwavenn@gmail.com>
2016-04-14 12:22:54 +0300

    Redone structure

--- github.rb
+++ github.rb
@@ -1,34 +1,46 @@
 require 'octokit'
+require 'yaml'
 
-config = YAML.load_file('config.yml')
-
-Octokit.configure do |c|
-  c.login = config['github_user']
-  c.password = config['github_user_password']
-end
-
-def get_tag_names(repo, tag_filter = '')
-  tags = []
-  Octokit.tags(repo).each do |current_tag|
-    tags << current_tag['name'] if current_tag['name'].include?(tag_filter)
+# Class for describing changes for github repo
+class GithubRepoChanges
+  def initialize(config_file: 'config.yml',
+                 repo: nil,
+                 tags_filter: 'onlyoffice-documentserver')
+    @config = YAML.load_file(config_file)
+    @repo = repo
+    @tags_filter = tags_filter
+    Octokit.configure do |c|
+      c.login = @config['github_user']
+      c.password = @config['github_user_password']
+    end
   end
-  tags
-end
 
-def get_changes_url(repo)
-  new_tags = get_tag_names(repo, 'onlyoffice-documentserver')
-  @new_tag = new_tags[0]
-  @old_tag = new_tags[1]
-  "https://github.com/#{repo}/compare/#{@old_tag}...#{@new_tag}"
-end
+  def tag_names(tag_filter = '')
+    tags = []
+    Octokit.tags(@repo).each do |current_tag|
+      tags << current_tag['name'] if current_tag['name'].include?(tag_filter)
+    end
+    tags
+  end
 
-def changes_empty?(repo)
-  changes = Octokit.compare(repo, @old_tag, @new_tag)
-  changes[:files].empty?
-end
+  def fetch_latest_tags
+    tags = tag_names(@tags_filter)
+    @new_tag = tags[0]
+    @old_tag = tags[1]
+  end
 
-def link_to_changes(repo)
-  changes_url = get_changes_url(repo)
-  return if changes_empty?(repo)
-  "[#{repo} changes](#{changes_url})\n"
+  def changes_url
+    "https://github.com/#{@repo}/compare/#{@old_tag}...#{@new_tag}"
+  end
+
+  def changes_empty?
+    changes = Octokit.compare(@repo, @old_tag, @new_tag)
+    changes[:files].empty?
+  end
+
+  def link_to_changes
+    fetch_latest_tags
+    return '' if changes_empty?
+    "[#{@repo} changes](#{changes_url})\n"
+  end
 end
