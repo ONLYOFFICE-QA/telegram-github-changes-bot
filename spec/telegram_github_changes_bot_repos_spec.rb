fc9ccb999811f0029e29e980050a8f945df0e8b0
Pavel Lobashov <shockwavenn@gmail.com>
2022-03-21 11:29:33 +0300

    Increase coverage

--- spec/telegram_github_changes_bot_repos_spec.rb
+++ spec/telegram_github_changes_bot_repos_spec.rb
@@ -6,4 +6,9 @@ describe TelegramGithubChangesBot, '#repos' do
   it 'bot#repos is array of GithubRepoChanges' do
     expect(changes_bot.repos).to all(be_a(GithubRepoChanges))
   end
+
+  it 'bot#repos is read only once after init' do
+    changes_bot.repos
+    expect(changes_bot.repos).to all(be_a(GithubRepoChanges))
+  end
 end
