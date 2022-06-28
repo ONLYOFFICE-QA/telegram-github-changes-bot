0e88946da064aff1f0daecc86ddd332a7f99ec2a
Pavel Lobashov <shockwavenn@gmail.com>
2022-06-28 13:22:50 +0300

    Fix rubocop issues

--- lib/telegram_github_changes_bot.rb
+++ lib/telegram_github_changes_bot.rb
@@ -56,12 +56,12 @@ class TelegramGithubChangesBot
 
   # @return [String] help message
   def help_message
-    "This bot is used to get changes between versions of DocumentServer\n"\
+    "This bot is used to get changes between versions of DocumentServer\n" \
       "-------\n" \
-      "Usage: <code>get_changes version1...version2</code>\n"\
+      "Usage: <code>get_changes version1...version2</code>\n" \
       "version1, version2 in format like <code>v6.4.1.45</code>\n" \
       "-------\n" \
-      "example: \n"\
+      "example: \n" \
       '/get_changes v6.5.0.46...v6.5.0.57'
   end
 
